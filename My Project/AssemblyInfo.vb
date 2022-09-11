2d1130264cb2eadd23f2c9153753a71457fe23e4
Bang have mental breakdown <75790567+Bang1338@users.noreply.github.com>
2022-09-11 08:55:06 +0700

    1.1

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Cookie Run Name Generator")> 
+<Assembly: AssemblyDescription("Generate custom cookie run name for you!")> 
+<Assembly: AssemblyCompany("BG Software")> 
+<Assembly: AssemblyProduct("Cookie Run Name Generator")> 
+<Assembly: AssemblyCopyright("No Copyright lol")> 
+<Assembly: AssemblyTrademark("Comic Studio")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("a6dc6b5f-8c36-4095-a05b-3fb98fa8bf58")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.1.0.0")> 
+<Assembly: AssemblyFileVersion("1.1.0.0")> 
